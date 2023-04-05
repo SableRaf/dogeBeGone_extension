fe3d6893cbe6ce9d56f6cbb37feced25b25cfdb9
Raphaël <groupes.raphael@gmail.com>
2023-04-05 17:24:26 +0200

    Always hide Doge
    Changed from Javascript injection to CSS. This makes sure that the logo is hidden from the page load.

--- contentScript.js
+++ contentScript.js
@@ -2,30 +2,30 @@
 // It is responsible for hiding the logo on the page
 // It targets the path element with the specified d attribute value
 // Note: This may be fragile and break if the logo is updated or the d attribute value changes
-function hideSpecifiedElement() {
-  const targetPathSelector = 'path[d="M2.412.974h19.176v22.052H2.412z"]';
-  const pathElement = document.querySelector(targetPathSelector);
+// function hideSpecifiedElement() {
+//   const targetPathSelector = 'path[d="M2.412.974h19.176v22.052H2.412z"]';
+//   const pathElement = document.querySelector(targetPathSelector);
 
-  if (pathElement) {
-    pathElement.style.visibility = "hidden";
-  }
-}
+//   if (pathElement) {
+//     pathElement.style.visibility = "hidden";
+//   }
+// }
 
-// Observe the DOM for changes and run the script when a change is detected
-function observeDOMChanges() {
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.type === "childList" && mutation.addedNodes.length) {
-        hideSpecifiedElement();
-      }
-    }
-  });
+// // Observe the DOM for changes and run the script when a change is detected
+// function observeDOMChanges() {
+//   const observer = new MutationObserver((mutations) => {
+//     for (const mutation of mutations) {
+//       if (mutation.type === "childList" && mutation.addedNodes.length) {
+//         hideSpecifiedElement();
+//       }
+//     }
+//   });
 
-  observer.observe(document.body, { childList: true, subtree: true });
-}
+//   observer.observe(document.body, { childList: true, subtree: true });
+// }
 
-// Add a listener to run the script whenever a change is detected
-window.onload = observeDOMChanges;
+// // Add a listener to run the script whenever a change is detected
+// window.onload = observeDOMChanges;
 
-// Run once to hide the logo on the initial page load
-hideSpecifiedElement();
+// // Run once to hide the logo on the initial page load
+// hideSpecifiedElement();
