4ecc2aae1e0070f5be10659bd1fb0256547b73c8
Raphaël <groupes.raphael@gmail.com>
2023-04-05 15:50:00 +0200

    Added comments

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,7 @@
+// This script is injected into the page and runs in the context of the page
+// It is responsible for hiding the logo on the page
+// It targets the path element with the specified d attribute value
+// Note: This may be fragile and break if the logo is updated or the d attribute value changes
 function hideSpecifiedElement() {
   const targetPathSelector = 'path[d="M2.412.974h19.176v22.052H2.412z"]';
   const pathElement = document.querySelector(targetPathSelector);
@@ -7,6 +11,7 @@ function hideSpecifiedElement() {
   }
 }
 
+// Observe the DOM for changes and run the script when a change is detected
 function observeDOMChanges() {
   const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
@@ -19,6 +24,8 @@ function observeDOMChanges() {
   observer.observe(document.body, { childList: true, subtree: true });
 }
 
+// Add a listener to run the script whenever a change is detected
 window.onload = observeDOMChanges;
 
+// Run once to hide the logo on the initial page load
 hideSpecifiedElement();
